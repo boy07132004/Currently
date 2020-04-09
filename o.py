76199242d6dc30f9806685238cf7eb63e4f61cd7
ZM_raspberry pi <boy07132004@gmail.com>
2020-04-09 11:39:04 +0800

    del jpg

--- o.py
+++ o.py
@@ -1,12 +1,12 @@
-from opc import Curr
+from test import Curr
 import time
 curr = Curr(3)
 #count = 0
 while 1:
     #count+=1
     time_1 = time.perf_counter()
-    val = (curr.read())
+    val = (curr.read2())
     time_2 = time.perf_counter()
     print(f'{val} - {1/(time_2-time_1):.1f} Hz-{time.strftime("%H-%M-%S")}')
-    time.sleep(0.1)
+    time.sleep(0.5)
 print(time.strftime("%H-%M-%S"))
